04bb73b4ae8788198377d0f66b0bae7a90ca8923
Kaspar <kaspar.wolfisberg@gmail.com>
2021-05-01 20:24:11 +0200

    Fix argument naming in converters.

--- rt_pie/utils/converters/__init__.py
+++ rt_pie/utils/converters/__init__.py
@@ -1,14 +1,13 @@
 import mir_eval
 
 
-def convert_cent_to_hz(cent, fref=10.0):
-    return fref * 2 ** (cent / 1200.0)
+def convert_cent_to_hz(cent, f_ref=10.0):
+    return f_ref * 2 ** (cent / 1200.0)
 
 
-def convert_hz_to_cent(herz, fref=10.0):
-    return mir_eval.melody.hz2cents(herz, fref)
+def convert_hz_to_cent(hertz, f_ref=10.0):
+    return mir_eval.melody.hz2cents(hertz, f_ref)
 
 
-def convert_semitone_to_hz(semi, fref=10.0):
-    return convert_cent_to_hz(100 * semi, fref)
-
+def convert_semitone_to_hz(semi, f_ref=10.0):
+    return convert_cent_to_hz(100 * semi, f_ref)
