d2deccfdb1d6f2ab291db142af4c1386068b1762
Kaspar <kaspar.wolfisberg@gmail.com>
2021-05-02 15:02:48 +0200

    Extend converters package.

--- rt_pie/utils/converters/__init__.py
+++ rt_pie/utils/converters/__init__.py
@@ -1,3 +1,4 @@
+import numpy as np
 import mir_eval
 
 
@@ -11,3 +12,43 @@ def convert_hz_to_cent(hertz, f_ref=10.0):
 
 def convert_semitone_to_hz(semi, f_ref=10.0):
     return convert_cent_to_hz(100 * semi, f_ref)
+
+
+def convert_bin_to_weighted_average_cents(label, octaves=6, cents_per_bin=20, hz_lower_bound=32.7):
+    classifier_lowest_cent = convert_hz_to_cent(np.array([hz_lower_bound]))[0]
+    classifier_total_bins = int((1200 / cents_per_bin) * octaves)
+    classifier_cents = np.linspace(
+        0, (classifier_total_bins - 1) * cents_per_bin, classifier_total_bins) + classifier_lowest_cent
+
+    if label.ndim == 1:
+        product_sum = np.sum(label * classifier_cents)
+        weight_sum = np.sum(label)
+        return product_sum / weight_sum
+    if label.ndim == 2:
+        product_sum = np.dot(label, classifier_cents)
+        weight_sum = np.sum(label, axis=1)
+        return product_sum / weight_sum
+    raise Exception("Label should be either 1d or 2d ndarray.")
+
+
+def convert_bin_to_local_average_cents(salience, center=None):
+    """
+    find the weighted average cents near the argmax bin
+    """
+    if not hasattr(convert_bin_to_local_average_cents, 'cents_mapping'):
+        # the bin number-to-cents mapping
+        convert_bin_to_local_average_cents.cents_mapping = (
+                np.linspace(0, 7180, 360) + 1997.3794084376191)
+    if salience.ndim == 1:
+        center = int(np.argmax(salience))
+        start = max(0, center - 4)
+        end = min(len(salience), center + 5)
+        salience = salience[start:end]
+        product_sum = np.sum(
+            salience * convert_bin_to_local_average_cents.cents_mapping[start:end])
+        weight_sum = np.sum(salience)
+        return product_sum / weight_sum
+    if salience.ndim == 2:
+        return np.array([convert_bin_to_local_average_cents(salience[i, :]) for i in
+                         range(salience.shape[0])])
+    raise Exception("Label should be either 1d or 2d ndarray.")
