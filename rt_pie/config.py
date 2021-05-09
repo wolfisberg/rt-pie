fbfb6d7d315237d51bc492450d785933c4a71947
Kaspar <kaspar.wolfisberg@gmail.com>
2021-05-09 16:55:07 +0200

    Move audio device prompt to config module.

--- rt_pie/config.py
+++ rt_pie/config.py
@@ -1,4 +1,32 @@
+import sounddevice as sd
+
+
 SAMPLE_RATE = 16000
 NUM_CHANNELS = 1
 BLOCK_SIZE = 1024
 BATCH_SIZE = 32
+
+
+def __check_device_selection(devices, selection):
+    try:
+        device_number = int(selection)
+        if device_number in range(0, len(devices)):
+            return device_number
+        raise ValueError()
+    except Exception as e:
+        print(f"Invalid input, expected number between 0 and {len(devices) - 1}.")
+        exit(-1)
+
+
+def prompt_audio_device(msg):
+    audio_devices = sd.query_devices()
+    print(msg)
+    print("Available audio devices:")
+    if len(audio_devices) < 2:
+        print(f"Using the only available audio device:")
+        print(audio_devices[0]["name"])
+        return audio_devices[0]
+    print(audio_devices)
+    selection = input('Choose your audio device: ')
+    print("\n")
+    return __check_device_selection(audio_devices, selection)
--- rt_pie/microphone_processor.py
+++ rt_pie/microphone_processor.py
@@ -1,18 +1,7 @@
-"""
-todo
-load trained model
-load audio file
-format audio to match model
-make prediction
-if path_f0:
-    compare predictions with f0
-"""
-
-
 import numpy as np
 import sounddevice as sd
 
-from rt_pie.rt_pie_lib import config
+from rt_pie import config
 
 
 def process_microphone(args):
@@ -21,30 +10,6 @@ def process_microphone(args):
     plot = None
     audio_recording = None
     return pitch_estimations, plot, audio_recording
-
-
-def __check_device_selection(devices, selection):
-    try:
-        device_number = int(selection)
-        if device_number in range(0, len(devices)):
-            return device_number
-        raise ValueError()
-    except Exception as e:
-        print(f"Invalid input, expected number between 0 and {len(devices) - 1}.")
-        exit(-1)
-
-
-def get_audio_device():
-    audio_devices = sd.query_devices()
-    print("Available audio devices:")
-    if len(audio_devices) < 2:
-        print(f"Using the only available audio device:")
-        print(audio_devices[0]["name"])
-        return audio_devices[0]
-    print(audio_devices)
-    selection = input('Choose your audio device: ')
-    print("\n")
-    return __check_device_selection(audio_devices, selection)
 
 
 def callb(indata, frames, time, status):
@@ -72,9 +37,9 @@ recording = []
 
 
 if __name__ == "__main__":
-    audio_in = get_audio_device()
+    audio_in = config.prompt_audio_device("")
     record(audio_in)
-    audio_out = get_audio_device()
+    audio_out = config.prompt_audio_device("")
     sd.play(recording, samplerate=config.SAMPLE_RATE, device=audio_out)
     status = sd.wait()
     print("Done.")
