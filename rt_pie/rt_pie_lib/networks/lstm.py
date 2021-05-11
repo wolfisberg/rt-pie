e858d384171ffe8fb4a6af25e255f8086976bfd5
Kaspar <kaspar.wolfisberg@gmail.com>
2021-05-12 01:29:51 +0200

    Add lstm model for checkpoint conversion.

--- rt_pie/rt_pie_lib/networks/lstm.py
+++ rt_pie/rt_pie_lib/networks/lstm.py
@@ -1,3 +1,16 @@
+import tensorflow as tf
+
+
 def get_model_lstm(block_size):
-    raise NotImplementedError()
-    return None
+    frame_step = block_size // 2
+    sample_length = 3
+    sample_rate = 16000
+    num_frames = 1 + (sample_rate * sample_length - block_size) // frame_step
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(num_frames, block_size)))
+    model.add(tf.keras.layers.LSTM(128, return_sequences=True))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(1, activation='relu'))
+    model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+    return model
