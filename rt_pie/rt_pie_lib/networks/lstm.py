135fb7cdd5e4a824ad450d21619af2896e81647b
Kaspar <kaspar.wolfisberg@gmail.com>
2021-05-12 10:19:38 +0200

    Fix lstm model dimensions.

--- rt_pie/rt_pie_lib/networks/lstm.py
+++ rt_pie/rt_pie_lib/networks/lstm.py
@@ -7,9 +7,10 @@ def get_model_lstm(block_size):
     sample_rate = 16000
     num_frames = 1 + (sample_rate * sample_length - block_size) // frame_step
 
+    output_dimension = 256
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(num_frames, block_size)))
-    model.add(tf.keras.layers.LSTM(128, return_sequences=True))
+    model.add(tf.keras.layers.LSTM(output_dimension, return_sequences=True, input_shape=(num_frames, block_size)))
+    model.add(tf.keras.layers.LSTM(output_dimension, return_sequences=True))
     model.add(tf.keras.layers.Dense(128, activation='relu'))
     model.add(tf.keras.layers.Dense(1, activation='relu'))
     model.compile(loss='mse', optimizer='adam', metrics=['mse'])
