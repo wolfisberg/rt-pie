ef7525987378837a0d5672a031cf796890fcefb9
Luca <lucadilanzo@hotmail.com>
2021-06-01 16:45:36 +0200

    add serialized models

--- rt_pie/fitted_models.py
+++ rt_pie/fitted_models.py
@@ -24,18 +24,24 @@ models = [
                 os.path.join(config.MODELS_BASE_PATH, "lstm_512_256_e065_model.hdf5")),
     FittedModel("LSTM_256", 256, True, "LSTM, block size 256, hop size 128",
                 os.path.join(config.MODELS_BASE_PATH, "lstm_256_128_e092_model.hdf5")),
+    FittedModel("CREPE_2048", 2048, False, "CREPE, block size 2048, hop size 1024",
+                os.path.join(config.MODELS_BASE_PATH, "crepe_2048_1024_e100_model.hdf5")),
     FittedModel("CREPE_1024", 1024, False, "CREPE, block size 1024, hop size 512",
                 os.path.join(config.MODELS_BASE_PATH, "crepe_1024_512_e100_model.hdf5")),
     FittedModel("CREPE_512", 512, False, "CREPE, block size 512, hop size 256",
                 os.path.join(config.MODELS_BASE_PATH, "crepe_512_256_e100_model.hdf5")),
     FittedModel("CREPE_256", 256, False, "CREPE, block size 256, hop size 128",
                 os.path.join(config.MODELS_BASE_PATH, "crepe_256_128_e084_model.hdf5")),
+    FittedModel("DEEPF0_2048", 2048, False, "DEEPF0, block size 2048, hop size 1024",
+                os.path.join(config.MODELS_BASE_PATH, "deepf0_2048_1024_e100_model.hdf5")),
     FittedModel("DEEPF0_1024", 1024, False, "DEEPF0, block size 1024, hop size 512",
                 os.path.join(config.MODELS_BASE_PATH, "deepf0_1024_512_e087_model.hdf5")),
     FittedModel("DEEPF0_512", 512, False, "DEEPF0, block size 512, hop size 256",
                 os.path.join(config.MODELS_BASE_PATH, "deepf0_512_256_e048_model.hdf5")),
     FittedModel("DEEPF0_256", 256, False, "DEEPF0, block size 256, hop size 128",
-                os.path.join(config.MODELS_BASE_PATH, "deepf0_256_128_e091_model.hdf5"))
+                os.path.join(config.MODELS_BASE_PATH, "deepf0_256_128_e091_model.hdf5")),
+    FittedModel("DEEPF0_1024_reduced", 1024, False, "DEEPF0, block size 1024, hop size 512, reduced filters",
+                os.path.join(config.MODELS_BASE_PATH, "deepf0_1024_512_reduced_filter_e100_model.hdf5"))
 ]
 
 
